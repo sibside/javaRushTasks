239b93e8bad0503e7195314566fdf6852da89c0f
Denis <denisidorenko@gmail.com>
2019-11-10 20:30:55 +0500

    JavaRush 8 lvl all complete tasks
    add all complete tasks

---
+++ JavaSyntax_9_level/task0906/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task09.task0906;
+
+/*
+Логирование стек-трейса
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        log("In main method");
+    }
+
+    public static void log(String s) {
+        //напишите тут ваш код
+        StackTraceElement[] element = Thread.currentThread().getStackTrace();
+        System.out.println(element[2].getClassName()+": "+element[2].getMethodName()+": "+s);
+    }
+}
