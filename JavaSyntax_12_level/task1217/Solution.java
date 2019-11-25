65c68003d16afaf2163361cf82f4d419f2b7b17a
Denis <denisidorenko@gmail.com>
2019-11-25 22:10:08 +0500

    All tasks of level 12
    add complete task

---
+++ JavaSyntax_12_level/task1217/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+//add interfaces here - добавь интерфейсы тут
+    public interface CanFly{
+        void Fly();
+    }
+    public interface CanRun{
+        void Run();
+    }
+    public interface CanSwim{
+        void Swim();
+    }
+}
