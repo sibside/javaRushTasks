c23943b4237e84b25fd03a1689d24365425d0710
Nemocoder <denisidorenko@gmail.com>
2020-01-23 22:15:00 +0500

    java rush complete tasks

--- task14/task1408/Hen.java
+++
@@ -1,9 +0,0 @@
-package com.javarush.task.task14.task1408;
-
-public abstract class Hen {
-        abstract int getCountOfEggsPerMonth();
-        String getDescription(){
-            return "Я - курица.";
-        }
-}
-
--- task15/task1529/Plane.java
+++
@@ -1,6 +0,0 @@
-package com.javarush.task.task15.task1529;
-
-public class Plane implements CanFly {
-    Plane (int maxPeople){}
-    public void fly(){}
-}
