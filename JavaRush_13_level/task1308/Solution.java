700c2c0a007e998b7bf9abd7a5f4313227d6f614
Nemocoder <denisidorenko@gmail.com>
2020-02-05 09:15:40 +0500

    13 level complete tasks

---
+++ JavaRush_13_level/task1308/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task13.task1308;
+
+/* 
+Эй, ты там живой?
+*/
+
+public class Solution {
+    interface Person{
+        boolean isAlive();
+    }
+
+    interface Presentable extends Person{
+
+    }
+    public static void main(String[] args) throws Exception {
+    }
+}
