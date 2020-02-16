7a7da3039276d6b32228f1a00b6affdd07c0bdc7
Nemocoder <denisidorenko@gmail.com>
2020-02-16 22:44:39 +0500

    16 level complete tasks

---
+++ JavaRush_16_level/task1622/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task16.task1622;
+
+/* 
+Последовательные выполнения нитей
+*/
+
+public class Solution {
+    public volatile static int COUNT = 4;
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < COUNT; i++) {
+            SleepingThread sleepingThread = new SleepingThread();
+            //напишите тут ваш код
+            try{
+                sleepingThread.join();
+            }catch (InterruptedException e){
+            }
+        }
+    }
+
+    public static class SleepingThread extends Thread {
+        private static volatile int threadCount = 0;
+        private volatile int countdownIndex = COUNT;
+
+        public SleepingThread() throws InterruptedException {
+            super(String.valueOf(++threadCount));
+            start();
+        }
+
+        public void run() {
+            while (true) {
+                System.out.println(this);
+                if (--countdownIndex == 0) return;
+                //напишите тут ваш код
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    System.out.println("Нить прервана");
+                }
+            }
+        }
+
+        public String toString() {
+            return "#" + getName() + ": " + countdownIndex;
+        }
+    }
+}
